505ab6a65735052ef5812fe371ca92bce20e1cad
Keli <thorkell@me.com>
2015-10-27 10:50:34 +0000

    flot

--- RuFanWeb/app/controllers/Application.java
+++ RuFanWeb/app/controllers/Application.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import is.rufan.team.service.GameService;
 import is.rufan.tournament.domain.Tournament;
+import is.rufan.tournament.domain.TournamentGame;
+import is.rufan.tournament.service.TournamentGameService;
 import is.rufan.tournament.service.TournamentService;
 import is.rufan.user.domain.User;
 import org.springframework.context.ApplicationContext;
@@ -17,20 +20,32 @@ import java.util.List;
 public class Application extends Controller {
 
     protected ApplicationContext tctx = new FileSystemXmlApplicationContext("/conf/tournamentapp.xml");
+    protected ApplicationContext tgctx = new FileSystemXmlApplicationContext("/conf/tournamentgameapp.xml");
+    protected ApplicationContext gctx = new FileSystemXmlApplicationContext("/conf/gameapp.xml");
+
+    private TournamentService tournamentService;
+    private TournamentGameService tournamentGameService;
+    private GameService gameService;
+
+    public Application(){
+        tournamentService = (TournamentService) tctx.getBean("tournamentService");
+        tournamentGameService = (TournamentGameService) tgctx.getBean("tournamentGameService");
+        gameService = (GameService) gctx.getBean("gameService");
+    }
     /**
      *
      * @return
      */
     public Result index()
     {
-        TournamentService tournamentService = (TournamentService) tctx.getBean("tournamentService");
         List<Tournament> tournamentList = tournamentService.getTournaments();
+
         List<Tournament> activeTournaments = new ArrayList<Tournament>();
         Date today = new Date();
 
         for(Tournament t : tournamentList){
             if(t.getStartDate() != null || t.getEndDate() != null){
-                if(t.getStartDate().before(today) && t.getEndDate().after(today)){
+                if(t.getEndDate().after(today)){
                     activeTournaments.add(t);
                 }
 
--- RuFanWeb/app/controllers/FantasyTeamController.java
+++ RuFanWeb/app/controllers/FantasyTeamController.java
@@ -1,10 +1,16 @@
 package controllers;
 
 import is.rufan.fantasyplayer.domain.FantasyPlayer;
+import is.rufan.fantasyplayer.service.FantasyPlayerService;
 import is.rufan.player.domain.Player;
 import is.rufan.player.service.PlayerService;
+import is.rufan.team.domain.Game;
+import is.rufan.team.service.GameService;
 import is.rufan.tournament.domain.Tournament;
+import is.rufan.tournament.domain.TournamentGame;
+import is.rufan.tournament.service.TournamentGameService;
 import is.rufan.tournament.service.TournamentService;
+import is.rufan.user.service.UserService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 import play.data.Form;
@@ -12,6 +18,7 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.tournament;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -23,77 +30,93 @@ import static play.data.Form.form;
 public class FantasyTeamController extends Controller {
     protected ApplicationContext tctx = new FileSystemXmlApplicationContext("/conf/tournamentapp.xml");
     protected ApplicationContext pctx = new FileSystemXmlApplicationContext("/conf/playerapp.xml");
-    //protected ApplicationContext fctx = new FileSystemXmlApplicationContext("/conf/fantasyplayerapp.xml");
+    protected ApplicationContext fctx = new FileSystemXmlApplicationContext("/conf/fantasyplayerapp.xml");
+    protected ApplicationContext uctx = new FileSystemXmlApplicationContext("/conf/userapp.xml");
+    protected ApplicationContext tgctx = new FileSystemXmlApplicationContext("/conf/tournamentgameapp.xml");
+    protected ApplicationContext gctx = new FileSystemXmlApplicationContext("/conf/gameapp.xml");
+
+
+    private PlayerService playerService;
+    private TournamentService tournamentService;
+    private FantasyPlayerService fantasyPlayerService;
+    private UserService userService;
+    private TournamentGameService tournamentGameService;
+    private GameService gameService;
 
     final static Form<FantasyPlayer> signupForm = form(FantasyPlayer.class);
 
+    public FantasyTeamController(){
+        playerService = (PlayerService) pctx.getBean("playerService");
+        tournamentService = (TournamentService) tctx.getBean("tournamentService");
+        fantasyPlayerService = (FantasyPlayerService) fctx.getBean("fantasyPlayerService");
+        userService = (UserService) uctx.getBean("userService");
+        tournamentGameService = (TournamentGameService) tgctx.getBean("tournamentGameService");
+        gameService = (GameService) gctx.getBean("gameService");
+    }
+
     public Result tournament(int id){
-        PlayerService playerService = (PlayerService) pctx.getBean("playerService");
         List<Player> players = playerService.getPlayers();
-        TournamentService tournamentService = (TournamentService) tctx.getBean("tournamentService");
         Tournament t = tournamentService.getTournamentById(id);
         Date today = new Date();
         if(t != null && t.getEndDate() != null && t.getStartDate() != null && t.getStartDate().before(today) && t.getEndDate().after(today)) {
-            return ok(tournament.render(t, players));
+            return ok(tournament.render(t, players, null));
         }else{
             return redirect("/PageNotFound");
         }
     }
 
     public Result buildFantasyTeam(int tournamentId){
-        PlayerService playerService = (PlayerService) pctx.getBean("playerService");
+
         List<Player> players = playerService.getPlayers();
-        TournamentService tournamentService = (TournamentService) tctx.getBean("tournamentService");
+
+
         Tournament t = tournamentService.getTournamentById(tournamentId);
+
+        if(t == null){
+            return redirect("/PageNotFound");
+        }
+
+        /*
+        TournamentGame tournamentGame = new TournamentGame();
+
+        for(Tournament tournament : tournamentService.getTournaments()){
+            List<Game> games = gameService.getGames();
+            for(Game game : games){
+                tournamentGame.setGameId(game.getGameId());
+                tournamentGame.setTournamentId(tournament.getId());
+                tournamentGameService.addTournamentGame(tournamentGame);
+            }
+        }
+        */
+
+        int userId = userService.getUserByUsername(session().get("username")).getId();
+
         Date today = new Date();
+        FantasyPlayer fantasyPlayer = new FantasyPlayer();
+        fantasyPlayer.setTournamentId(tournamentId);
+        fantasyPlayer.setUserId(userId);
 
         Form<FantasyPlayer> filledForm = signupForm.bindFromRequest();
-        if(filledForm.field("GoalKeeper").value() != null) {
-            System.out.println(filledForm.field("GoalKeeper").value());
+        if(!filledForm.field("GoalKeeper").value().equals("-")) {
+            fantasyPlayer.setPlayerId(Integer.parseInt(filledForm.field("GoalKeeper").value()));
+            //fantasyPlayerService.addFantasyPlayer(fantasyPlayer);
         }
 
-        if(filledForm.field("Defender1").value() != null) {
-            System.out.println(filledForm.field("Defender1").value());
-        }
-
-        if(filledForm.field("Defender2").value() != null) {
-            System.out.println(filledForm.field("Defender2").value());
-        }
-
-        if(filledForm.field("Defender3").value() != null) {
-            System.out.println(filledForm.field("Defender3").value());
-        }
-
-        if(filledForm.field("Defender4").value() != null) {
-            System.out.println(filledForm.field("Defender3").value());
-        }
-
-        if(filledForm.field("Midfielder1").value() != null) {
-            System.out.println(filledForm.field("Midfielder1").value());
-        }
-
-        if(filledForm.field("Midfielder2").value() != null) {
-            System.out.println(filledForm.field("Midfielder2").value());
-        }
-
-        if(filledForm.field("Midfielder3").value() != null) {
-            System.out.println(filledForm.field("Midfielder3").value());
-        }
-
-        if(filledForm.field("Midfielder4").value() != null) {
-            System.out.println(filledForm.field("Midfielder4").value());
-        }
-
-        if(filledForm.field("Forward1").value() != null) {
-            System.out.println(filledForm.field("Forward1").value());
-        }
-
-        if(filledForm.field("Forward2").value() != null) {
-            System.out.println(filledForm.field("Forward2").value());
+        List<FantasyPlayer> tournamentFantasyPlayers = fantasyPlayerService.getFantasyPlayersWithTournamentId(tournamentId);
+        List<Player> usersFantasyPlayers = new ArrayList<Player>();
+        Player newPlayer;
+        for(FantasyPlayer fp : tournamentFantasyPlayers){
+            if(fp.getUserId() == userId ){
+                for(Player p : players){
+                    if(p.getPlayerId() == fp.getPlayerId()){
+                        usersFantasyPlayers.add(p);
+                    }
+                }
+            }
         }
 
         if(t != null && t.getEndDate() != null && t.getStartDate() != null && t.getStartDate().before(today) && t.getEndDate().after(today)) {
-            return ok(tournament.render(t, players));
+            return ok(tournament.render(t, players, usersFantasyPlayers));
         }else{
             return redirect("/PageNotFound");
         }
